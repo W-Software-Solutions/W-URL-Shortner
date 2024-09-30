adad0263c48689dd87a94d7a9858a1cfcdc0bd06
W-Software-Solutions <154895656+W-Software-Solutions@users.noreply.github.com>
2024-09-30 23:01:08 +0530

    init commit

---
+++ src/Components/Container.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import FormContainer from './FormContainer';
+import { UrlData } from '../UrlData';
+import axios from 'axios';
+import DataTable from './DataTable';
+
+interface IContainerProps {
+}
+
+const Container: React.FunctionComponent<IContainerProps> = () => {
+  const [data, setdata] = React.useState<UrlData[]>([]);
+  const [reload, setreload] = React.useState<boolean>(false);
+
+  const updateReloadState = ():void => {
+    setreload(true); 
+  }
+
+  const fetchTableData = async () => {
+    const response = await axios.get('https://w-qjrm.onrender.com/');
+    // console.log("The response from the server is: ", response)
+    setdata(response.data);
+    setreload(false)
+  }
+
+  React.useEffect(()=>{
+    fetchTableData();
+
+  },[reload])
+
+
+  return(
+    <>
+    <FormContainer updateReloadState = {updateReloadState} />
+    <DataTable updateReloadState = {updateReloadState} data={data}/>
+    </>
+  ) ;
+};
+
+export default Container;
