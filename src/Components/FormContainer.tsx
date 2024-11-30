9f04cee4af82a9cc4c0decc4863d874e7fa97a19
W-Software-Solutions <154895656+W-Software-Solutions@users.noreply.github.com>
2024-11-30 23:10:32 +0530

    Update FormContainer.tsx

--- src/Components/FormContainer.tsx
+++ src/Components/FormContainer.tsx
@@ -32,15 +32,15 @@ const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
                 <form onSubmit={handleSubmit}>
                     <div className="flex">
                         <div className="relative w-full">
-                            <div className="w-auto h-full absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800 hidden sm:block "> urlShortner.link </div>
+                            {/* <div className="h-full absolute inset-y-3.5 start-0 items-center ps-2 pointer-events-none text-slate-800 sm:text-2xl "> urlShortner.link </div> */}
 
-                            <input type='text' placeholder='add your link' required
-                            className='block w-full p-4 ps-32 text-sm text-gray-900 border 
+                            <input type='text' placeholder='add your link ....' required
+                            className='block w-full p-4 ps-5 text-sm text-gray-900 border 
                              border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500' 
                              value={fullUrl} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setfullUrl(e.target.value)}}/>
 
                             <button type='submit' 
-                            className='absolute top-0 end-0 p-2.5 text-sm font-medium bg-black h-full sm:w-auto
+                            className='absolute top-0 end-0 p-2 text-sm font-medium bg-black h-full
                              text-white rounded-lg border-black focus:ring-4 focus:outline-none' >Shorten URL</button>
                         </div>
                     </div>
